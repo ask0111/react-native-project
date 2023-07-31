729b97f415e70649bd7ffe4d8eae5a7dd786f56c
Hariom Yadav <hariomask0111@gmail.com>
2023-07-31 18:17:52 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import { Text, View, StyleSheet, ScrollView, Modal } from 'react-native';
+import SignUp from './components/signup/SignUp';
+
+
+
+
+
+
+function App(){
+  
+  return (
+  <>
+      <SignUp />
+  </>
+      
+  );
+}
+
+const styles = StyleSheet.create({
+  
+  
+});
+
+export default App;
---
+++ components/signin/SignIn.js
@@ -0,0 +1,18 @@
+
+
+
+import { useState } from "react";
+import { StyleSheet } from "react-native";
+
+export default function SignIn(){
+    return(<>
+
+
+
+
+    </>)
+}
+
+const styles = StyleSheet.create({
+
+})
